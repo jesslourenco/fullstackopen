1f91dc2cf45f79c1e61e05f2febc928436764ac9
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-10-04 16:39:19 -0400

    phonebook 5 steps completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      phone: '224' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+
+  const handleAddName = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleAddNum = (event) => {
+    console.log(event.target.value)
+    setNewNum(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    if(persons.flatMap(e => e.name).includes(newName)){
+      alert(`${newName} already exists in phonebook`)
+      return(setNewName(''), setNewNum(''))
+    }
+
+    const person = { name : newName, phone : newNum }
+
+    setPersons(persons.concat(person))
+    setNewName('')
+    setNewNum('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+                  value={newName}
+                  onChange={handleAddName} />
+        </div>
+        <div>
+          number: <input 
+                    value={newNum}
+                    onChange={handleAddNum} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map(e => <li key={e.name}> {e.name} {e.phone} </li>)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
