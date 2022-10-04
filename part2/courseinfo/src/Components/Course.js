0e37e364ef09ef9c04f7d0d1e6377e8375101fc1
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-10-04 11:25:25 -0400

    courseinfo - fixed div element with key

--- part2/courseinfo/src/Components/Course.js
+++ part2/courseinfo/src/Components/Course.js
@@ -6,24 +6,24 @@ import Total from './Total'
 const Course = ({courses}) =>{
 
     return (
-    <div>
-        <h1>Web Development Curriculum</h1>
-            {courses.map(course => 
-                <div key={course.id}>
-                    <Header key={course.id} title={course.name} />
-                    {course.parts.map(part => 
-                        <Content key={part.id} topic={part.name} exercises={part.exercises} />
-                    )}
-                    <Total total={course.parts
-                        .map(e => e.exercises)
-                        .reduce((total, item) => 
-                            total + item, 0)}  
-                    />
-                </div>
-            )
-            }
-            
-    </div>
+        <div>
+            <h1>Web Development Curriculum</h1>
+                {courses.map(course => 
+                    <div key={course.id}>
+                        <Header title={course.name} />
+                        {course.parts.map(part => 
+                            <Content key={part.id} topic={part.name} exercises={part.exercises} />
+                        )}
+                        <Total total={course.parts
+                            .map(e => e.exercises)
+                            .reduce((total, item) => 
+                                total + item, 0)}  
+                        />
+                    </div>
+                )
+                }
+                
+        </div>
     )
 }
 export default Course
