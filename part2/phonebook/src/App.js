5ae7e76c00c9293cf4c5b6450c4a5ef41b88fd90
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-10-05 19:00:18 -0400

    phonebook with server step 6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import People from './components/People'
 import Filter from './components/Filter'
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-      { name: 'Arto Hellas', number: '040-123456'},
-      { name: 'Ada Lovelace', number: '39-44-5323523'},
-      { name: 'Dan Abramov', number: '12-43-234345'},
-      { name: 'Mary Poppendieck', number: '39-23-6423122'}
-    ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [filter, setFilter] = useState()
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response =>{
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleAddName = (event) => {
     setNewName(event.target.value)
