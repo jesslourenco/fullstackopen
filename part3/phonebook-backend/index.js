047bd5f9b193fa6cdb1b6dd47cc7b716eb2f18cb
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-10-09 13:23:24 -0400

    part 3 phonebook-backend step 3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -33,7 +33,6 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {  
-    console.log('route is correct')
     const info = {
         date: new Date(),
         numRecords: persons.length
@@ -45,6 +44,17 @@ app.get('/info', (request, response) => {
         </div>`
         )
   })
+
+app.get('/api/persons/:id', (request, response) => { 
+  const id = Number(request.params.id) 
+  const person = persons.find(p => p.id === id)
+
+ if (person) {  
+	 response.json(person)  
+ } else {   
+	 response.status(404).end() 
+ }
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
