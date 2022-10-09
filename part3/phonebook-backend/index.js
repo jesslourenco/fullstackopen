e3e30e27f0c72f97a595caf4ffbe6e9113e2e642
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-10-09 13:54:21 -0400

    phonebook-backend step 4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -56,6 +56,20 @@ app.get('/api/persons/:id', (request, response) => {
  }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+ 
+  const id = Number(request.params.id)
+
+  if (persons.find(p => p.id === id)) {  
+
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+    
+  } else {   
+    response.status(404).end()
+  }
+ })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
