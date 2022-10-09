df75dd911c6c2686771fd3f05f9fe1747aadffc9
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-10-09 18:52:26 -0400

    phonebook-backend step 9 build frontend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('body', (req) => {
-    return 'POST' === req.method ? JSON.stringify(req.body) : '' // special morgen token only for POST
+    return 'POST' === req.method ? JSON.stringify(req.body) : '' // special morgan token only for POST
   })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -111,7 +114,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
