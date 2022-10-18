6b3cd5e8bc2263a61786e406408dece789b28c30
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-10-17 22:45:31 -0400

    part 3 phonebook lint config

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 const Person = require('./models/person')
@@ -11,8 +10,8 @@ app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('body', (req) => {
-    return 'POST' === req.method ? JSON.stringify(req.body) : '' // special morgan token only for POST
-  })
+  return 'POST' === req.method ? JSON.stringify(req.body) : '' // special morgan token only for POST
+})
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -22,74 +21,74 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
   Person.find({})
-  .then(persons => {
-    response.json(persons)
-  })
+    .then(persons => {
+      response.json(persons)
+    })
 })
 
 
 app.get('/info', (request, response) => {
 
   Person.count({})
-   .then( result => {
-    const info = {
-      date: new Date(),
-      numRecords: result
-    }
+    .then(result => {
+      const info = {
+        date: new Date(),
+        numRecords: result
+      }
 
-    response.send(
-      `<div> Phonebook has info for ${info.numRecords} people
+      response.send(
+        `<div> Phonebook has info for ${info.numRecords} people
               <br>
               ${info.date}
           </div>`
-    )
-  })
+      )
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    response.json(person)
-  })
-  .catch(error => next(error))
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => next(error))
 
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
 
   Person.findByIdAndRemove(request.params.id)
-  .then(person => {
-    if (person) {
-      console.log(`${person.name} has been deleted`)
-      response.status(204).end()
-    } else {
-      response.status(404).end()
-    }
-    
-  })
-  .catch(error => next(error))
+    .then(person => {
+      if (person) {
+        console.log(`${person.name} has been deleted`)
+        response.status(204).end()
+      } else {
+        response.status(404).end()
+      }
+
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
 
-  const body = request.body 
+  const body = request.body
 
   if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
   const person = new Person({
-      name: body.name,
-      number: body.number
+    name: body.name,
+    number: body.number
   })
 
   person.save()
-  .then(person => {
-    response.json(person)
-  })
-  .catch((err) => next(err))
+    .then(person => {
+      response.json(person)
+    })
+    .catch((err) => next(err))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -98,10 +97,10 @@ app.put('/api/persons/:id', (request, response, next) => {
   const update = { number: body.number }
 
   Person.findByIdAndUpdate(request.params.id, update, { new: true, runValidators: true, context: 'query' })
-  .then(updatedPerson => {
-    response.json(updatedPerson)
-  })
-  .catch(error => next(error))
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -117,6 +116,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
