92db431f86ca6652a49fcc48d06cd6638a438697
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-11-01 11:51:15 -0300

    part 4 blog list unit tests, step 3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -3,7 +3,15 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((acc, blog) => acc + blog.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  if (!blogs.length) return 'No blogs in the array';
+  let top = { likes: 0 };
+  blogs.forEach((e) => { if (e.likes > top.likes) top = e; });
+  return top;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
