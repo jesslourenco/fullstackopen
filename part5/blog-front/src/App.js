c965ae74dbe151379ff47e060abfbbed0f410154
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-11-11 17:47:59 -0300

    part 5a step 1

--- part5/blog-front/src/App.js
+++ part5/blog-front/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
+import { useState, useEffect } from 'react';
+import Post from './components/post';
+import Notification from './components/notification';
+import Login from './components/login';
+import postService from './services/posts';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [user, setUser] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedPostappUser');
+    if (loggedUserJSON) {
+      const userData = JSON.parse(loggedUserJSON);
+      setUser(userData);
+      postService.setToken(userData.token);
+      postService.getAll().then((e) => setPosts(e));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Object.keys(user).length !== 0) {
+      postService.getAll().then((e) => setPosts(e));
+    }
+  }, [user]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Blog</h2>
+
+      <Notification message={errorMessage} />
+
+      {Object.keys(user).length === 0
+        ? (
+          <Login
+            setUser={setUser}
+            setErrorMessage={setErrorMessage}
+          />
+        )
+        : (
+          <div>
+            <p>
+              {user.name}
+              {' '}
+              logged-in
+            </p>
+            {posts.map((post) => <Post key={post.id} post={post} />)}
+          </div>
+        )}
+
     </div>
   );
 }
---
+++ part5/blog-front/src/components/login.js
@@ -0,0 +1,59 @@
+/* eslint-disable react/no-this-in-sfc */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+import { useState } from 'react';
+import loginService from '../services/login';
+import postService from '../services/posts';
+
+// eslint-disable-next-line react/prop-types
+function Login({ setUser, setErrorMessage }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const userForLogin = await loginService.login({
+        username, password,
+      });
+
+      window.localStorage.setItem('loggedPostappUser', JSON.stringify(userForLogin));
+
+      postService.setToken(userForLogin.token);
+      setUser(userForLogin);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+}
+
+export default Login;
