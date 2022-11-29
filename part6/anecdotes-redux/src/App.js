b8083220b0051e967e2905df3aacfa7cc2688295
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-11-29 12:15:21 -0300

    part 6a anecdotes step 6

--- part6/anecdotes-redux/src/App.js
+++ part6/anecdotes-redux/src/App.js
@@ -1,39 +1,13 @@
-/* eslint-disable no-console */
-/* eslint-disable react/jsx-indent */
-/* eslint-disable implicit-arrow-linebreak */
-/* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable react/jsx-wrap-multilines */
-/* eslint-disable react/button-has-type */
-import { useSelector, useDispatch } from 'react-redux';
-import { vote } from './reducers/anecdoteReducer';
+/* eslint-disable react/jsx-filename-extension */
 import AnecdoteForm from './components/AnecdoteForm';
+import AnecdoteList from './components/AnecdoteList';
 
 function App() {
-  const anecdotes = useSelector((state) => state);
-  // eslint-disable-next-line no-unused-vars
-  const dispatch = useDispatch();
-
-  const voteHandler = (id) => {
-    dispatch(vote(id));
-    console.log('vote', id);
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) =>
-      <div key={anecdote.id}>
-        <div>
-          {anecdote.content}
-        </div>
-        <div>
-          has
-          {' '}
-          {anecdote.votes}
-          <button onClick={() => voteHandler(anecdote.id)}>vote</button>
-        </div>
-      </div>)}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   );
---
+++ part6/anecdotes-redux/src/components/AnecdoteList.js
@@ -0,0 +1,34 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
+
+import { useSelector, useDispatch } from 'react-redux';
+import { vote } from '../reducers/anecdoteReducer';
+
+function AnecdoteList() {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const voteHandler = (id) => {
+    dispatch(vote(id));
+  };
+
+  return (
+    <div>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has
+            {' '}
+            {anecdote.votes}
+            <button type="submit" onClick={() => voteHandler(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AnecdoteList;
