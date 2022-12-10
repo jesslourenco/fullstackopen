5060907638b170edcc9d05e44b84786924eb044a
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-10 15:52:34 -0500

    ex 8.4 complete

--- part8/library/index.js
+++ part8/library/index.js
@@ -99,7 +99,7 @@ const typeDefs = gql`
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -108,7 +108,11 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if(!args) return books
+
+      return books.filter((b) => b.author === args.author)
+    },
     allAuthors: () => {
       const response =  authors.map((a) => {
         const count = books.filter((b) => b.author === a.name).length
