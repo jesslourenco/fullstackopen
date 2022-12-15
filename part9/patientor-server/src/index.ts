cb20cade90cb775dec7e89272221782d7be606fd
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-15 12:15:13 -0500

    part 9  ex 9.10 complete

--- part9/patientor-server/src/index.ts
+++ part9/patientor-server/src/index.ts
@@ -1,17 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouters from './routes/diagnoseRouters';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-
 const PORT = 3001;
 
-app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
-});
+
+app.use('/', diagnoseRouters);
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
