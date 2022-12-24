4b2c5b79d0f3517b930ee2a9beb87206d0a1dfcc
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-24 17:17:50 -0500

    fixed e2e test port

--- part11/full-stack-open-pokedex/cypress/support/commands.js
+++ part11/full-stack-open-pokedex/cypress/support/commands.js
@@ -3,26 +3,3 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-undef */
 /* eslint-disable */
-Cypress.Commands.add('login', ({ username, password }) => {
-  cy.request('POST', 'http://localhost:3003/api/login', {
-    username, password,
-  }).then(({ body }) => {
-    localStorage.setItem('loggedPostappUser', JSON.stringify(body));
-    cy.visit('http://localhost:3000');
-  });
-});
-
-Cypress.Commands.add('createPost', ({ title, author, url }) => {
-  cy.request({
-    url: 'http://localhost:3003/api/posts',
-    method: 'POST',
-    body: {
-      title, author, url
-    },
-    headers: {
-      Authorization: `bearer ${JSON.parse(localStorage.getItem('loggedPostappUser')).token}`,
-    },
-  });
-
-  cy.visit('http://localhost:3000');
-});
