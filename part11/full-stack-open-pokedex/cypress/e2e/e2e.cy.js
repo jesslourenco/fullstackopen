4f94d3e42160e80c424fb409f8520239f4625a12
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-24 16:47:24 -0500

    fixed e2e test port

--- part11/full-stack-open-pokedex/cypress/e2e/e2e.cy.js
+++ part11/full-stack-open-pokedex/cypress/e2e/e2e.cy.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
   it('pokemon page has correct content', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.get('a').contains('ivysaur').click()
     cy.url().should('include', '/pokemon/ivysaur')
     cy.contains('chlorophyll')
