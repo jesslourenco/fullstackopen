9f7fc9eb46d9c5543d4c71ba6be07a26e3be2905
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-24 18:04:09 -0500

    chnaged port on e2e tests

--- part11/full-stack-open-pokedex/cypress/e2e/e2e.cy.js
+++ part11/full-stack-open-pokedex/cypress/e2e/e2e.cy.js
@@ -1,13 +1,16 @@
 /* eslint-disable */
+require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit(`http://localhost:${PORT}`)
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
   it('pokemon page has correct content', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit(`http://localhost:${PORT}`)
     cy.get('a').contains('ivysaur').click()
     cy.url().should('include', '/pokemon/ivysaur')
     cy.contains('chlorophyll')
