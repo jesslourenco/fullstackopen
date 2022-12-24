73a2ff291bd13882c4f9e92b857e93851c50eb11
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-24 17:47:54 -0500

    port error

--- part11/full-stack-open-pokedex/app.js
+++ part11/full-stack-open-pokedex/app.js
@@ -1,8 +1,9 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
