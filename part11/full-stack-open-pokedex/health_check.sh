4f0d7aa3879f6a61ae51aa3dbba7f61d2e559dae
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-24 21:07:33 -0500

    part 11 ex 11.12 - actions should succeed

--- part11/full-stack-open-pokedex/health_check.sh
+++ part11/full-stack-open-pokedex/health_check.sh
@@ -2,4 +2,10 @@
 
 echo "Hello from shell script"
 
+check=$(curl http://localhost:8080/health)
+
+if [[ "$check" == "ok" ]]; then
 exit 0 # exit status 1 means that the script "fails"
+fi
+
+exit 1 
