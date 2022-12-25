56db5121a9123aec649fb1ecef08a366eb7e83cd
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-24 19:04:21 -0500

    part 11 ex 11.11

--- part11/full-stack-open-pokedex/app.js
+++ part11/full-stack-open-pokedex/app.js
@@ -11,3 +11,13 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port ', PORT)
 })
+
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('7') // change this string to ensure a new version deployed
+})
