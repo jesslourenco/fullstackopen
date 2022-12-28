04468dafea03a9c9dc54aa40657dcd4c5805827f
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-27 20:14:38 -0500

    part 12 ex 12.7 mongodb file added

--- part12/todo-app/todo-backend/mongo/mongo-init.js
+++ part12/todo-app/todo-backend/mongo/mongo-init.js
@@ -11,5 +11,5 @@ db.createUser({
 
 db.createCollection('todos');
 
-db.todos.insert({ text: 'Write code', done: true });
-db.todos.insert({ text: 'Learn about containers', done: false });
+db.todos.insertOne({ text: 'Write code', done: true });
+db.todos.insertOne({ text: 'Learn about containers', done: false });
