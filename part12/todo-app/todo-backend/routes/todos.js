fbed72ab7171667986b52bb54e28c576cd9cd203
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-27 23:16:12 -0500

    part 12 ex 12.7 fixed put/:id and get/:id

--- part12/todo-app/todo-backend/routes/todos.js
+++ part12/todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,14 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = req.todo
+  res.send(todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  await Todo.updateOne({_id: req.todo.id}, { text: req.body.text, done: req.body.done })
+  res.sendStatus(204);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
