4e32a1ae47a36a03add91b0c97e938d6ba821197
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-28 00:08:05 -0500

    part 12 ex 12.9 redis setup

--- part12/todo-app/todo-backend/util/config.js
+++ part12/todo-app/todo-backend/util/config.js
@@ -3,5 +3,5 @@ const REDIS_URL = process.env.REDIS_URL || undefined
 
 module.exports = {
   MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
-  REDIS_URL//: '//localhost:6378'
+  REDIS_URL//: 'redis://localhost:6378'
 }
