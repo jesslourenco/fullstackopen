de01d8fd7b5dca0e1a7bfb0c563211a9cd648855
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-30 21:50:16 -0500

    part 13 ex 13.4 restful

--- part13/blog-backend-with-postgresql/index.js
+++ part13/blog-backend-with-postgresql/index.js
@@ -1,16 +1,67 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-const main = async () => {
-    try {
-      await sequelize.authenticate()
-      console.log('Connection has been established successfully.')
-      sequelize.close()
-    } catch (error) {
-      console.error('Unable to connect to the database:', error)
-    }
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
   }
-  
-main()
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync()
+
+app.get('/api/posts', async (req, res) => {
+  const posts = await Blog.findAll()
+  res.json(posts)
+})
+
+app.use(express.json())
+
+app.post('/api/posts', async (req, res) => {
+  try {
+    const post = await Blog.create(req.body)
+    return res.json(post)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/posts/:id', async (req, res) => {
+  try {
+    await Blog.destroy({ where: {id: req.params.id } })
+    return res.status(204).json()
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
