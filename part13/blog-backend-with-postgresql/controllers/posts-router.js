c1004876a9049fc944a382dedc5cb00386453c9c
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-12-31 02:30:53 -0500

    part 13 ex 13.10 token validation for post request on blogs

--- part13/blog-backend-with-postgresql/controllers/posts-router.js
+++ part13/blog-backend-with-postgresql/controllers/posts-router.js
@@ -1,14 +1,31 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
 require('express-async-errors');
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
+const { SECRET } = require('../util/config')
 
 router.get('/', async (req, res) => {
     const posts = await Blog.findAll()
     res.json(posts)
 })
 
-router.post('/', async (req, res) => {
-    const post = await Blog.create(req.body)
+const tokenExtractor = (req, res, next) => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        try {
+            req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+        } catch {
+            return res.status(401).json({ error: 'token invalid' })
+        }
+    } else {
+        return res.status(401).json({ error: 'token missing' })
+    }
+    next()
+}
+
+router.post('/', tokenExtractor, async (req, res) => {
+    const user = await User.findByPk(req.decodedToken.id)
+    const post = await Blog.create({...req.body, userId: user.id})
     return (
         post
             ? res.json(post)
